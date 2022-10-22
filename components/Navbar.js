a0f39a67f9bbf2677b0bd0dc5f2490563e1d4510
Wajeeha Mushtaq <wajeehamushtaq1@gmail.com>
2022-10-22 17:29:47 +0500

    Add Home page component

---
+++ components/Navbar.js
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="logo">
+        <Image src="/logo.png" alt="site logo" width={80} height={80} />
+      </div>
+      <Link href="/"><a>Home</a></Link>
+      <Link href="/"><a>Dashboard</a></Link>
+      <Link href="/"><a>Leaderboard</a></Link>
+    </nav>
+  );
+}
+ 
+export default Navbar;
